6be2db833b5271e83d8ea64924a75dd839b6c4ba
SongSeungGyu <sorim831@naver.com>
2025-01-28 23:55:09 +0900

    feat     |	대기 사용자 승인 및 거절 기능 추가

---
+++ backend/models/rejecteduser.js
@@ -0,0 +1,29 @@
+// models/rejecteduser.js
+const mongoose = require("mongoose");
+
+const rejecteduserSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
+  }, // 이메일 필드 추가
+  password: { type: String, required: true }, // 해시 형태로 저장
+  nickName: { type: String, required: true, unique: true },
+  isAdmin: { type: Boolean, default: false }, // 기본값 false
+  status: {
+    type: String,
+    enum: ["normal", "manager", "professor"], // 상태 값 단순화
+    default: "normal",
+  },
+  profileImage: { type: String, default: null }, // 프로필 이미지 URL
+  achievements: { type: [String], default: [] }, // 업적
+  interests: { type: [String], default: [] }, // 관심사
+
+  reason: { type: String }, // 거절 사유
+});
+
+const rejectedUser = mongoose.model("rejectedUser", rejecteduserSchema);
+
+module.exports = rejectedUser;
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const tempUser = require("../models/tempuser");
+const rejectedUser = require("../models/rejecteduser");
 const jwt = require("jsonwebtoken");
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
@@ -21,6 +23,13 @@ router.post("/login", authController.login);
 
 // 아이디 중복 체크 엔드포인트
 router.get("/check-id", authController.checkIdAvailability);
+
+// 회원가입 대기 엔드포인트
+router.get("/get-temp-users", authController.getTempUsers);
+// 회원가입 승인 엔드포인트
+router.get("/approve", authController.approveUser);
+// 회원가입 거절 엔드포인트
+router.get("/reject", authController.rejectUser);
 
 // 유저 정보 조회 엔드포인트(토큰 필요)
 router.get("/me", authMiddleware, authController.getUserInfo);
