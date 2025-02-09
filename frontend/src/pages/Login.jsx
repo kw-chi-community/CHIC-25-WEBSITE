7e173f469b4e28633efaf846925004e9f69a939b
SongSeungGyu <sorim831@naver.com>
2025-02-09 21:05:20 +0900

    feat     | 로그인 API 구현 및 프론트엔드와 연결

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import "../styles/Login.css";
 
 const Login = () => {
+  const address = process.env.REACT_APP_BACKEND_ADDRESS;
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: "",
@@ -19,16 +20,52 @@ const Login = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
+    try {
+      const response = await fetch(`${address}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: formData.username,
+          password: formData.password,
+        }),
+      });
+
+      const result = await response.json();
+
+      if (!response.ok) {
+        // 서버에서 에러 메시지를 보냈을 경우 처리
+        throw new Error(result.error || "로그인이 실패했습니다");
+      }
+
+      if (result.token) {
+        localStorage.setItem("token", result.token); // 토큰 저장
+        alert("로그인 성공! 홈 화면으로 이동합니다.");
+        navigate("/");
+      } else {
+        //alert(result.message); // 실패 메시지 표시
+
+        setError(result.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError(error.message);
+      alert("로그인이 실패했습니다.");
+    }
+
     // 여기에 로그인 처리
+    /*
     if (formData.username === "admin" && formData.password === "1234") {
       alert("로그인 성공! 홈 화면으로 이동합니다.");
       navigate("/");
     } else {
       setError("아이디 또는 비밀번호가 잘못되었습니다.");
     }
+      */
   };
 
   return (
